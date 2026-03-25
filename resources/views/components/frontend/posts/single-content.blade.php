50acfdf80f2f1fcdd723147242d8257faacb09ac
Mac <mac@MacBook-Pro-4.local>
2026-03-25 19:16:30 +0100

    postdetailpagina afwerken

--- resources/views/components/frontend/posts/single-content.blade.php
+++ resources/views/components/frontend/posts/single-content.blade.php
@@ -1,1 +1,25 @@
-<?php
+@props(['post'])
+
+<div class="gazette-single-post-details">
+    <div class="blog-post-thumbnail mb-50">
+        @if($post->media)
+            <img src="{{ $post->media->url() }}" alt="{{ $post->title }}">
+        @endif
+    </div>
+
+    <div class="single-post-content">
+        <div class="gazette-post-tag">
+            @foreach($post->categories as $category)
+                <a href="{{ route('categories.show', $category) }}">{{ $category->name }}</a>
+            @endforeach
+        </div>
+        <h2 class="font-pt mb-30">{{ $post->title }}</h2>
+        <p class="gazette-post-date">
+            <span>Door: {{ $post->user->name }}</span> | <span>{{ optional($post->published_at)->format('F d, Y') }}</span>
+        </p>
+
+        <div class="post-body single-post-text">
+            {!! $post->body !!}
+        </div>
+    </div>
+</div>
--- resources/views/frontend/posts/show.blade.php
+++ resources/views/frontend/posts/show.blade.php
@@ -12,29 +12,9 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-8">
-                    <div class="gazette-single-post-details">
-                        <div class="blog-post-thumbnail mb-50">
-                            @if($post->media)
-                                <img src="{{ $post->media->url() }}" alt="{{ $post->title }}">
-                            @endif
-                        </div>
+                    <x-frontend.posts.single-content :post="$post" />
 
-                        <div class="single-post-content">
-                            <div class="gazette-post-tag">
-                                @foreach($post->categories as $category)
-                                    <a href="{{ route('categories.show', $category) }}">{{ $category->name }}</a>
-                                @endforeach
-                            </div>
-                            <h2 class="font-pt mb-30">{{ $post->title }}</h2>
-                            <p class="gazette-post-date">
-                                <span>Door: {{ $post->user->name }}</span> | <span>{{ optional($post->published_at)->format('F d, Y') }}</span>
-                            </p>
-
-                            <div class="post-body">
-                                {!! $post->body !!}
-                            </div>
-                        </div>
-                    </div>
+                    <x-frontend.posts.discussion-area :post="$post" />
                 </div>
             </div>
         </div>
