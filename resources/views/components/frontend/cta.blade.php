75343cba0f86b471e94c8a4b4ba57c4bbe856352
Mac <mac@MacBook-Pro-4.local>
2026-03-26 18:21:15 +0100

    code verdeeld in componenten

--- resources/views/components/frontend/cta.blade.php
+++ resources/views/components/frontend/cta.blade.php
@@ -1,1 +1,9 @@
-<?php
+<div class="row">
+    <div class="col-12">
+        <div class="about-us-content text-center mb-50 p-5 bg-dark text-white rounded">
+            <h2 class="font-pt text-white mb-30">Wil je deel uitmaken van ons verhaal?</h2>
+            <p class="mb-30 text-slate-300">We zijn altijd op zoek naar gepassioneerde schrijvers, data-analisten en creatieve geesten om ons team te versterken of hun verhalen met ons te delen.</p>
+            <a href="{{ route('contact') }}" class="btn gazette-btn">Neem Contact Met Ons Op</a>
+        </div>
+    </div>
+</div>
